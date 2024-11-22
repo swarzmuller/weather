9ef581c35aed3362d5763a7cebbf7c6c9e84f24a
Vitalii Ilchenko <vitalii@Vitaliis-MacBook-Air.local>
2024-11-22 19:05:46 +0200

    Allow Cors

--- weather-backend/src/main.ts
+++ weather-backend/src/main.ts
@@ -6,6 +6,12 @@ const server = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://swarzmuller.github.io', // Replace with your frontend domain
+    methods: 'GET,POST,PUT,DELETE', // Specify allowed HTTP methods
+    allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers
+    credentials: true, // Enable if cookies or authorization headers are required
+  });
   await app.listen(process.env.PORT ?? 3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
