42f0ba1a664e433bdd886a2bf873546ddbc15d15
Vitalii <vitalii.ichenko@gmail.com>
2024-11-22 21:18:16 +0200

    To Render

--- weather-backend/src/main.ts
+++ weather-backend/src/main.ts
@@ -7,7 +7,7 @@ const server = express();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://swarzmuller-github-io.onrender.com/',
+    origin: 'https://swarzmuller-github-io.onrender.com',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
