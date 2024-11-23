70814e69fb4568dfa1dfba79496bd0c1a24954f4
Vitalii Ilchenko <vitalii@Vitaliis-MacBook-Air.local>
2024-11-23 11:18:02 +0200

    Return

--- weather-backend/src/main.ts
+++ weather-backend/src/main.ts
@@ -1,26 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
-import * as serverlessHttp from 'serverless-http';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
 
 const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'https://swarzmuller-github-io.onrender.com',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
-  await app.init(); // Initialize the app without starting the listener
-  // await app.listen(process.env.PORT ?? 3001);
-  // console.log(`Application is running on: ${await app.getUrl()}`);
-  console.log(`Application initialized`);
+  await app.listen(process.env.PORT ?? 3001);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
 
-// Export the serverless handler
-export const handler = serverlessHttp(server);
+export default server;
