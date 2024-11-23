efc11c678110fb18c6346b4272e4253048f262ea
Vitalii Ilchenko <vitalii@Vitaliis-MacBook-Air.local>
2024-11-23 10:48:53 +0200

    Vercel setup

--- weather-backend/src/main.ts
+++ weather-backend/src/main.ts
@@ -1,21 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
+import * as serverlessHttp from 'serverless-http';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
     origin: 'https://swarzmuller-github-io.onrender.com',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3001);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.init(); // Initialize the app without starting the listener
+  // await app.listen(process.env.PORT ?? 3001);
+  // console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Application initialized`);
 }
 
 bootstrap();
 
-export default server;
+// Export the serverless handler
+export const handler = serverlessHttp(server);
